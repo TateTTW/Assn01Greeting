572dbe56e161edd0667191eb34c272b72114a2f6
tatet <tatet@LAPTOP-CS9CTK12.home>
2019-09-01 20:40:35 -0400

    Improved spacing and added more text

--- JavaScript/date.js
+++ JavaScript/date.js
@@ -4,7 +4,7 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
-today = mm + '/' + dd + '/' + yyyy;
+today = ' ' + mm + '/' + dd + '/' + yyyy;
 
 document.getElementById('dateToday').innerHTML = today;
 }
